cd40cc3d59f6c59d127e0c368292c72b01df687c
DonnieWest <Igneo676@gmail.com>
2013-05-26 19:22:43 -0500

    Added additional Case Classes. Need to make all this return a List of Hashmaps, then feed them into my DB.
    TODO: make individual objects that deal with the types of Posts my app is capable of handling

--- src/com/donniewest/titan/Client.scala
+++ src/com/donniewest/titan/Client.scala
@@ -30,36 +30,81 @@ object Client {
   def retrieve_your_posts = {
     //TODO: make method capable of handling various post numbers, maybe even post types?
     val json_post_feed = parse(HttpRequest.get(Endpoints.getPost_feed).accept("application/vnd.tent.posts-feed.v0+json").authorization(Hawk_Headers.build_headers_after_authentication("","GET",Endpoints.getPost_feed, "application/vnd.tent.post.v0+json")).body)
+
+    /*
+Case classes implemented:
+
+"entity"
+"post_id"
+"app_name"    X
+"post_type"
+"mentions"    X
+"published"
+"content"     X
+"location"
+"media"
+     */
     case class App(name: String, url: String) {
 
       def getInfo = List(name, url)
 
     }
+
+    case class Mentions(mentions: List[String]) {
+
+      def getMentions = mentions
+
+    }
+
+    object Mentions {
+
+      def apply: Mentions = Mentions(List(""))
+
+    }
+
     case class Content(text: String) {
 
       def getText = text
 
     }
 
+    case class ID(id: String) {
 
-    case class Post(app: App , content: Content){
-
-      def getInfo = List(app.getInfo(0),app.getInfo(1), content.getText)
+      def getId = id //TODO: Test. Make sure it doesn't extract both the Post ID and the Post version ID
 
     }
 
-    case class Data(data: List[Post]) {
+    case class Published(published_at: BigInt) {
 
-      def getData = data.map(i => List(i.getInfo(0), i.getInfo(1), i.getInfo(2)))
+      def getTime = published_at
+
+    }
+
+    case class Type(`type`: String) {
+
+      def getType = `type`
+
+    }
+
+    case class Status_Post(app: App , content: Content, mentions: Mentions){
+
+      def getInfo = List(app.getInfo(0),app.getInfo(1), content.getText, mentions.getMentions)    //TODO: Convert to Hashmap, include the rest of this craziness
+
+    }
+
+    case class Status_Posts(data: List[Status_Post]) {
+
+      def getData = data.map(i => List(i.getInfo(0), i.getInfo(1), i.getInfo(2)))    //TODO: Make this a list of HashMaps
 
     }
 
     implicit val formats = DefaultFormats
 
-    json_post_feed.extract[Data]
-    val mDbHelper = new Feed_database(getContext)
+    json_post_feed.extract[Status_Posts]
+
+/*    val mDbHelper = new Feed_database(getContext)
     val db = mDbHelper.getWritableDatabase
-    val values = new ContentValues
+    val values = new ContentValues*/
 
   }
 
