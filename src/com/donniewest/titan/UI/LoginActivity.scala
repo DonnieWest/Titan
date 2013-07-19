c2347119ec011218664d4a8ea4e5d2d2efff15cc
igneo676 <igneo676@gmail.com>
2013-07-19 14:49:29 -0500

    Retrieve post ahead of time

--- src/com/donniewest/titan/UI/LoginActivity.scala
+++ src/com/donniewest/titan/UI/LoginActivity.scala
@@ -24,6 +24,7 @@ class LoginActivity extends SherlockActivity with SActivity {
 //        openUri(redirection)
 // The above ^ will be relevant when Tent 0.3 is released and the user has to go through the Oauth Flow. For now, nah
         error("Do I get this far?")
+        Client.retrieve_feed(getApplicationContext)
         startActivity[PostFeedActivity]
 
 
