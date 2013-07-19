87a59c8c2a14c0789c76d41338ae317d193d7a34
igneo676 <igneo676@gmail.com>
2013-07-19 14:44:34 -0500

    Add Activity for our Post Feed

---
+++ src/com/donniewest/titan/UI/PostFeedActivity.scala
@@ -0,0 +1,28 @@
+package UI
+
+import android.os.Bundle
+import com.orm.query.Select
+import DB.Posts
+import org.scaloid.common._
+
+class PostFeedActivity extends SActivity{
+
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+
+    val posts = Select.from(classOf[Posts]).orderBy("Published").list().toArray()
+    val adapter = new SArrayAdapter(posts)
+
+    setContentView(
+
+      new SVerticalLayout{
+
+        SListView().setAdapter(adapter)
+
+      }
+
+    )
+
+
+    }
+}
