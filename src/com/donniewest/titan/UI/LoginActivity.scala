a5cf24015f539047f7ae66359573187363313eb4
igneo676 <igneo676@gmail.com>
2013-07-24 22:36:19 -0500

    Rip Out ActionBarSherlock, convert to powerful Scaloid layouts

--- src/com/donniewest/titan/UI/LoginActivity.scala
+++ src/com/donniewest/titan/UI/LoginActivity.scala
@@ -1,36 +1,40 @@
 package UI
 
 import tent.Client
-import com.actionbarsherlock.app.SherlockActivity
 import com.donniewest.titan.R
 import android.widget.{TextView, Button}
 import org.scaloid.common._
 import android.os.Bundle
 import scala.concurrent.ops._
-import android.util.Log
-import android.content.Intent
 
-class LoginActivity extends SherlockActivity with SActivity {
+
+class LoginActivity extends SActivity {
 
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.login_activity)
-    find[Button](R.id.b_signin).onClick {
+    setContentView(
+      new SVerticalLayout{
 
-      spawn{
 
-        val entity = find[TextView](R.id.et_password).getText.toString
-        Client.authenticateServer(entity, getApplicationContext)
-//        openUri(redirection)
-// The above ^ will be relevant when Tent 0.3 is released and the user has to go through the Oauth Flow. For now, nah
-        error("Do I get this far?")
-        Client.retrieveFeed(getApplicationContext)
-        startActivity[PostFeedActivity]
+          val pass = SEditText() inputType TEXT_PASSWORD
+          SButton("Sign In").onClick{
 
+            spawn{
+
+            val entity = pass.text.toString
+            Client.authenticateServer(entity, getApplicationContext)
+            /*        openUri(redirection)
+            This will be relevant when Tent 0.3 is released and the user has to go through the Oauth Flow*/
+            Client.retrieveFeed(getApplicationContext)
+            startActivity[PostFeedActivity]
+
+
+          }}
 
       }
+
+    )
+
     }
 
   }
-
-}
