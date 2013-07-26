1e415f0d702f0dee8009e3d4faf7823c2717a4ee
igneo676 <igneo676@gmail.com>
2013-07-25 20:38:05 -0500

    Add basic webview activity
    Eventually to serve as authentication point

---
+++ src/com/donniewest/titan/UI/LoginWebView.scala
@@ -0,0 +1,21 @@
+package UI
+
+import android.os.Bundle
+import org.scaloid.common._
+
+
+class LoginWebView extends SActivity {
+    override def onCreate(savedInstanceState: Bundle) {
+      super.onCreate(savedInstanceState)
+      setContentView{
+
+        new SVerticalLayout{
+
+          SWebView()
+
+        }
+
+
+      }
+    }
+}
